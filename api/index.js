f26b3ea729c9458cb7816f8448a4a7243e7a9e61
Odogwu <becomingadev@gmail.com>
2023-09-07 04:51:46 +0100

    Create connection to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import conn from './config/db.js';
 
 const app = express();
+
+conn();
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
