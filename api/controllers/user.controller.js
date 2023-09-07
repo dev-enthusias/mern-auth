86fa4567d9c0c4e5b26b829e14d581ab7123daba
Odogwu <becomingadev@gmail.com>
2023-09-07 23:36:04 +0100

    Create userRouter

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const userController = (req, res) => {
+  res.json({ message: 'Api is working' });
+};
+
+export { userController };
--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import conn from './config/db.js';
+import userRouter from './routes/user.route.js';
 
 const app = express();
 
 conn();
 
+app.use('/api/users/', userRouter);
+
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
+
