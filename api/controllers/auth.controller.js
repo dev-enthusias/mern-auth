6f9a3c5a7b34212bb6d88bc2fa572cd06b30c72c
Odogwu <becomingadev@gmail.com>
2023-09-09 18:41:58 +0100

    Create sign API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import errorHandler from '../utils/error.js';
 
 const signUp = async function (req, res, next) {
@@ -14,4 +15,29 @@ const signUp = async function (req, res, next) {
   }
 };
 
-export default signUp;
+const signIn = async function (req, res, next) {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });  
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+
+    const { password: hashPassword, ...others } = validUser._doc;
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_KEY);
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 3600000),
+      })
+      .status(201)
+      .json(others);
+  } catch (error) {
+    next(error);
+    // res.status(200).json({message: 'Pls work'})
+  }
+};
+
+export { signUp, signIn };
