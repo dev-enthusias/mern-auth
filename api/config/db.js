6aefdc0dbb3461cf5d2394a9c41e67b9b639c1b9
Odogwu <becomingadev@gmail.com>
2023-09-09 15:27:55 +0100

    Add signup page functionality

--- api/config/db.js
+++ api/config/db.js
@@ -6,6 +6,7 @@ dotenv.config();
 const conn = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log('Connected to the database');
   } catch (error) {
     console.log(error.message);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,44 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    setLoading(true);
+    setError(false);
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+
+
+    if (data) setLoading(false);
+
+    if (data.success === false) return setError(true);
+  };
+
   return (
     <div className='max-w-lg mx-auto p-3'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='Username'
           id='username'
           name='username'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='email'
@@ -18,6 +46,7 @@ export default function SignUp() {
           id='email'
           name='email'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='password'
@@ -25,13 +54,23 @@ export default function SignUp() {
           id='password'
           name='password'
           className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
         />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          Sign up
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 disabled:cursor-not-allowed'
+        >
+          {loading ? 'Loading...' : 'Sign up'}
         </button>
       </form>
       <p className='flex gap-2 mt-5'>
-        Already have an account? <Link to='/signin' className='text-blue-600'>Sign in</Link>
+        Already have an account?{' '}
+        <Link to='/signin' className='text-blue-600'>
+          Sign in
+        </Link>
+      </p>
+      <p className='text-red-500 mt-5'>
+        {error && 'This user already exists!'}
       </p>
     </div>
   );
