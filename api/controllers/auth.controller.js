11cbd3e1de551c8bd7e942c34281332de7887371
Odogwu <becomingadev@gmail.com>
2023-09-09 21:21:00 +0100

    Create signin page UI and add page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -18,7 +18,7 @@ const signUp = async function (req, res, next) {
 const signIn = async function (req, res, next) {
   const { email, password } = req.body;
   try {
-    const validUser = await User.findOne({ email });  
+    const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found'));
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
@@ -36,7 +36,6 @@ const signIn = async function (req, res, next) {
       .json(others);
   } catch (error) {
     next(error);
-    // res.status(200).json({message: 'Pls work'})
   }
 };
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = e => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -22,10 +23,10 @@ export default function SignUp() {
     });
     const data = await res.json();
 
-
     if (data) setLoading(false);
 
     if (data.success === false) return setError(true);
+    navigate('/signin');
   };
 
   return (
