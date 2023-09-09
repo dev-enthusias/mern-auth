2f832b8245f5173bfa7a1f0cd6f8fd8100cfc429
Odogwu <becomingadev@gmail.com>
2023-09-09 08:13:31 +0100

    Create error handler function and middleware

--- api/index.js
+++ api/index.js
@@ -11,6 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
